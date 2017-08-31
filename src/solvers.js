25183e1aa4ec558d9d414fc8e98108aabc0b0b70
nehacp <neha.chaudhary@hotmail.com>
2017-08-30 21:01:24 -0700

    finished findNRockSolution()

--- src/solvers.js
+++ src/solvers.js
@@ -13,19 +13,48 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-
-
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var solution = [];
+  //if n = 1
+  //start a for loop to create rows with rooks at unique position
+  for(let i = 0; i < n; i++) {
+    //create new array with 0s;
+    var temp = new Array(n).fill(0);
+    //place the rook at index i
+    temp[i] = 1;
+    //push to solution
+    solution.push(temp);
+  }
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  //create nxn board
+  var grid = new Board({n: n});
+  var board = grid.attributes;
+  var boardSize = grid.get('n');
+  var rooks = n;
 
+  function findPosition() {
+    if(rooks === 0) {
+      solutionCount++;
+      return;
+    }
+    //iterate over board
+    for(let i = 0; i < boardSize; i++) {
+      //Check if board is valid
+      findPosition(i);
+      if(!grid.hasRowConflictAt(i) && !grid.hasColConflictAt(i)) {
+        board[i][i] = 1
+        rooks--;
+      }
+    }
+  }
+  findPosition();
+  //base case: index is creater than board size or number of rooks = n
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
